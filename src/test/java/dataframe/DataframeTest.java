a069b627c5009debc13a21581e9271f9a3b65204
SekhmetOrb <romano.s5@laposte.net>
2020-04-30 10:21:39 +0200

    nettoyage

---
+++ src/test/java/dataframe/DataframeTest.java
@@ -0,0 +1,14 @@
+package dataframe;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DataframeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
